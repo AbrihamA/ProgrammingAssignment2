a4c528429a55c844ad6b6477fb0ff879d3999d66
Abriham Akele <abriham.akele@gmail.com>
2016-04-04 15:13:11 +0300

    catchmatrix modified and complete, final

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## the first function "makeCacheMatrix" returns a list object which is composed of
 ## some functions which are used to set and return a matrix and its inverse. it will also catches the inverse
 ## a call to this function should pass an invertible matrix to "makeCacheMatrix" 
-## eg. x<-makeCacheMatrix(y)    where y is an invertible matrix and gets the list object returned by the function 
+## eg. x<-makeCacheMatrix(y)    where y is an invertible matrix and x gets the list object returned by the function 
 ## and x will be passed to the second function "cacheSolve"
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -20,7 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## a call to this function should pass what is returned by the first function. 
 ## eg cacheSolve(x)    where x is an object returned by the first function.
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the ## matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
 	im <- x$getinverse()
